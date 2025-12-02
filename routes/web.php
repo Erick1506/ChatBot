86cd353cf420e046b822228d8e497feb9bea96f6
Your Name <you@example.com>
2025-12-02 14:36:54 -0500

    bre

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function() { 
+    return view('home'); 
+});
 
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('login'); //resources/views/login.blade.php
 });
 
