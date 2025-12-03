3c858e93fad434104f63d5bbe9753eeefa86eada
Your Name <you@example.com>
2025-12-03 09:24:36 -0500

    probando el pdf final

--- resources/views/certificados/plantilla.blade.php
+++ resources/views/certificados/plantilla.blade.php
@@ -37,7 +37,7 @@
             position: absolute;
             left: 0;
             top: 0;
-            margin-top: -95px;
+            margin-top: -60px;
             margin-left:43%
         }
         
